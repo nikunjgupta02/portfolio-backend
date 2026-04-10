6064fea5e88a23fcd4f395a5a5ad99ac383e805b
Nikunj-Sphinx <nikunj@sphinx-solution.com>
2026-04-10 14:45:27 +0530

    fixed

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Mail; // Import the Mail Facade
 
 class ContactController extends Controller
 {
-   public function handleContact(Request $request) 
+public function handleContact(Request $request) 
 {
     $data = $request->validate([
         'name' => 'required|string|max:255',
@@ -18,26 +18,22 @@ class ContactController extends Controller
     ]);
 
     try {
-        // 1. Notify YOU about the new message
-        Mail::to('nikunjguptank00@gmail.com')->send(new ContactMessage($data));
-
-
-        // 2. Send an Auto-Reply to THEM
-        // We can pass a slightly different subject for them
-        Mail::to($data['email'])->send(new ContactMessage([
-            'name' => $data['name'],
-            'email' => 'nikunjguptank00@gmail.com', 
-            'subject' => 'Confirmation: I received your message!',
-            'message' => "Hi {$data['name']}, thanks for reaching out! This is an automated confirmation to let you know I've received your message regarding '{$data['subject']}'. I'll get back to you as soon as possible."
-        ]));
+        // We only send ONE email to minimize the risk of a 30s timeout.
+        // We add a 'replyTo' so you can respond directly from your inbox.
+        Mail::to('nikunjguptank00@gmail.com')
+            ->send((new ContactMessage($data))->replyTo($data['email'], $data['name']));
 
         return response()->json([
             'status' => 'success',
-            'message' => 'Messages sent successfully!'
+            'message' => 'Message sent successfully!'
         ], 200);
 
     } catch (\Exception $e) {
-        return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        // If it still fails, the error message will appear in the JSON response
+        return response()->json([
+            'status' => 'error', 
+            'message' => 'Mail server error: ' . $e->getMessage()
+        ], 500);
     }
 }
 }
