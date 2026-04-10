91fa98d5db7a9a751c299bf52d6231c519121380
Nikunj-Sphinx <nikunj@sphinx-solution.com>
2026-04-10 14:06:12 +0530

    Fix: Update to correct email address

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -19,13 +19,14 @@ class ContactController extends Controller
 
     try {
         // 1. Notify YOU about the new message
-        Mail::to('nikunjgupta02@gmail.com')->send(new ContactMessage($data));
+        Mail::to('nikunjguptank00@gmail.com')->send(new ContactMessage($data));
+
 
         // 2. Send an Auto-Reply to THEM
         // We can pass a slightly different subject for them
         Mail::to($data['email'])->send(new ContactMessage([
             'name' => $data['name'],
-            'email' => 'nikunjgupta02@gmail.com', // Your contact info
+            'email' => 'nikunjguptank00@gmail.com', 
             'subject' => 'Confirmation: I received your message!',
             'message' => "Hi {$data['name']}, thanks for reaching out! This is an automated confirmation to let you know I've received your message regarding '{$data['subject']}'. I'll get back to you as soon as possible."
         ]));
